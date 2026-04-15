22ac9fe7a38b638e138d799d773f4568bf01ad01
IgorVarani <igor.duo@aluno.senai.br>
2026-04-15 11:12:04 -0300

    :sparkles: feat: finalizando tela login

---
+++ vh-burguer/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+//? ESTRUTURA PADRÃO
+
+import { Fragment } from "react/jsx-runtime"
+
+const Home = () => {
+    return (
+        <Fragment>
+            <p>AAA</p>
+        </Fragment>
+    )
+}
+
+export default Home;
---
+++ vh-burguer/src/pages/login/index.tsx
@@ -0,0 +1,30 @@
+import { Fragment } from "react/jsx-runtime";
+import styles from "./login.module.css"
+
+//? ESTRUTURA PADRÃO
+const Login = () => {
+    return (
+        <Fragment>
+            <main id={styles.main}>
+                <img src="/imgs/Hamburguer_Login.png" alt="Hambúrguer com ingredientes flutuando."/>
+                <div id={styles.campo_login}>
+                    <h1>Login</h1>
+                    <form id={styles.formulario}>
+                        <div className={styles.campo_form}>
+                            <label htmlFor="email">E-mail</label>
+                            <input type="text" name="email" placeholder="email@exemplo.com"/>
+                        </div>
+                        <div className={styles.campo_form}>
+                            <label htmlFor="senha">Senha</label>
+                            <input type="password" name="senha" placeholder="********"/>
+                        </div>
+                        <a href="" id={styles.esq_senha}>Esqueceu sua senha?</a>
+                        <button>Entrar</button>
+                    </form>
+                </div>
+            </main>
+        </Fragment>
+    );
+}
+
+export default Login;
